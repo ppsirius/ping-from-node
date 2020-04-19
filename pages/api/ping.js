e2d5e25dedd3721793f7b162aed6ceeb8ae3e4cb
Piotr Pupczyk <ppsirius@gmail.com>
2020-04-19 21:23:01 +0200

    add colors in terminal

--- pages/api/ping.js
+++ pages/api/ping.js
@@ -1,3 +1,5 @@
+import db from "../../utils/lowdb";
+const chalk = require("chalk");
 const ping = require("ping");
 const hosts = ["google.com"];
 const frequency = "2000";
@@ -11,9 +13,24 @@ const hostsLoop = () => {
         extra: ["-i", "2"],
       })
       .then(function (res) {
-        console.log(res.avg, res.host, new Date().toLocaleString());
+        addEntityLog(new Date().toLocaleString(), res.avg, res.host);
+        console.log(
+          chalk.inverse(`  ${new Date().toLocaleString()}  `),
+          chalk.magenta(res.avg),
+          chalk.gray(res.host)
+        );
       });
   });
 };
 
-setInterval(hostsLoop, frequency);
+const addEntityLog = (date, ping, host) => {
+  db.get("log").push({ timestamp: date, ping: ping, host: host }).write();
+};
+
+export default (req, res) => {
+  if (req.method === "GET") {
+    setInterval(hostsLoop, frequency);
+  } else {
+    new Error("Unhandled method");
+  }
+};
