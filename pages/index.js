6cc049be2229003eea7c2c69f688340dd0f12d70
Piotr Pupczyk <ppsirius@gmail.com>
2020-04-19 20:27:19 +0200

    add baseweb lib

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,36 @@
-import React from "react";
-import Link from "next/link";
+import { Card, StyledBody, StyledAction } from "baseui/card";
+import { Button } from "baseui/button";
+import { Slider } from "baseui/slider";
 
-export default () => (
-  <ul>
-    <h2>Next</h2>
-    <li>
-      <Link href="/a" as="/b">
-        <a>b</a>
-      </Link>
-    </li>
-  </ul>
-);
+import { Block } from "baseui/block";
+import { H5 } from "baseui/typography";
+
+export default () => {
+  const [value, setValue] = React.useState([2]);
+
+  return (
+    <div>
+      <Block width={["600px"]} backgroundColor="primary200">
+        <Card>
+          <H5>Ping Settings</H5>
+          <span>ping interval in seconds</span>
+          <Slider
+            value={value}
+            onChange={({ value }) => value && setValue(value)}
+            onFinalChange={({ value }) => console.log(value)}
+          />
+
+          <StyledAction>
+            <Button
+              overrides={{
+                BaseButton: { style: { width: "100%" } },
+              }}
+            >
+              Start Ping Test
+            </Button>
+          </StyledAction>
+        </Card>
+      </Block>
+    </div>
+  );
+};
