f1ab7ba9fbd458460032d460ee34b708e58f71ea
Piotr Pupczyk <ppsirius@gmail.com>
2020-04-25 14:37:30 +0200

    get last log

--- pages/api/logs.js
+++ pages/api/logs.js
@@ -4,9 +4,22 @@ const getLogs = () => {
   return db.get("log").sortBy("timestamp");
 };
 
+const getLastLog = () => {
+  const logsLength = db.get("log").size().value();
+  return db.get(`log[${logsLength - 1}]`).value();
+};
+
 export default (req, res) => {
   if (req.method === "GET") {
-    res.end(JSON.stringify(getLogs()));
+    const {
+      query: { filter },
+    } = req;
+
+    if (filter === "last") {
+      res.end(JSON.stringify(getLastLog()));
+    } else {
+      res.end(JSON.stringify(getLogs()));
+    }
   } else {
     new Error("Unhandled method");
   }
